cdf4774c89d422cc3d0d6b127adcc6fdeb741abf
kaiquesilvadev <j.kaique1570@gmail.com>
2023-11-21 23:18:13 -0300

    validação de senha em usuario

--- src/main/java/com/kaique/ifood/dto/request/UsuarioDtoRequest.java
+++ src/main/java/com/kaique/ifood/dto/request/UsuarioDtoRequest.java
@@ -3,6 +3,7 @@ package com.kaique.ifood.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,7 @@ public class UsuarioDtoRequest {
 	@Email
 	private String email;
 	
+	@Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z]).*$")
 	@NotNull
 	@Size(min = 6 , max = 15)
 	private String senha;
