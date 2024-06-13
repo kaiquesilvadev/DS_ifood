3cbbe08221e0fac196dc7a2fc0a45f9fd0252d19
kaiquesilvadev <j.kaique1570@gmail.com>
2024-06-13 20:51:29 -0300

    retorno de dto para RestauranteFormaPagamentoController

--- src/main/java/com/kaique/ifood/controlles/RestauranteFormaPagamentoController.java
+++ src/main/java/com/kaique/ifood/controlles/RestauranteFormaPagamentoController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kaique.ifood.entities.FormaPagamento;
+import com.kaique.ifood.dto.conversor.FormaPagamentoDtoConversor;
+import com.kaique.ifood.dto.responce.FormaPagamentoDtoResponce;
 import com.kaique.ifood.services.RestauranteFormaPagamentoService;
 
 @RestController
@@ -22,9 +23,13 @@ public class RestauranteFormaPagamentoController {
 	@Autowired
 	private RestauranteFormaPagamentoService serviceFP;
 	
+	@Autowired
+	private FormaPagamentoDtoConversor conversorFP;
+
+	
 	@GetMapping
-	public List<FormaPagamento> listaFormaPagamento(@PathVariable Long restauranteId) {
-		return serviceFP.ListaFormaPagamento(restauranteId);
+	public List<FormaPagamentoDtoResponce> listaFormaPagamento(@PathVariable Long restauranteId) {
+		return conversorFP.listaDto(serviceFP.ListaFormaPagamento(restauranteId));
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
@@ -35,7 +40,7 @@ public class RestauranteFormaPagamentoController {
 	
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/{formaPagamentoId}")
-	public List<FormaPagamento> addFormaPagamentoEmRestaurante(@PathVariable Long restauranteId ,@PathVariable Long formaPagamentoId) {
-		return serviceFP.addFormaPagamentoEmRestaurante(restauranteId, formaPagamentoId);
+	public List<FormaPagamentoDtoResponce> addFormaPagamentoEmRestaurante(@PathVariable Long restauranteId ,@PathVariable Long formaPagamentoId) {
+		return conversorFP.listaDto(serviceFP.addFormaPagamentoEmRestaurante(restauranteId, formaPagamentoId));
 	}
 }
